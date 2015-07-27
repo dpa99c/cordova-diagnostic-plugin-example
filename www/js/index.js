10624aa86ae99d12d0b5e8380f9ece0592f52041
Dave Alden <dpa99c@gmail.com>
2015-07-27 22:14:57 +0100

    Initial version

---
+++ www/js/index.js
@@ -0,0 +1,51 @@
+
+
+function onDeviceReady() {
+    // Bind events
+    $(document).on("resume", onResume);
+    $('#do-check').on("click", checkState);
+    $('#settings #location-settings').on("click", onClickLocationSettings);
+    checkState();
+}
+
+
+function checkState(){
+    console.log("Checking state...");
+
+    $('#state li').removeClass('on off');
+
+    cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
+        $('#state .location').addClass(enabled ? 'on' : 'off');
+    }, onError);
+
+    cordova.plugins.diagnostic.isLocationEnabledSetting(function(enabled){
+        $('#state .location-setting').addClass(enabled ? 'on' : 'off');
+    }, onError);
+
+    cordova.plugins.diagnostic.isLocationAuthorized(function(enabled){
+        $('#state .location-authorisation').addClass(enabled ? 'on' : 'off');
+    }, onError);
+
+    cordova.plugins.diagnostic.isWifiEnabled(function(enabled){
+        $('#state .wifi').addClass(enabled ? 'on' : 'off');
+    }, onError);
+
+    cordova.plugins.diagnostic.isCameraEnabled(function(enabled){
+        $('#state .camera').addClass(enabled ? 'on' : 'off');
+    }, onError);
+}
+
+function onError(error){
+    console.error("An error occurred: "+error);
+}
+
+function onClickLocationSettings(){
+    cordova.plugins.diagnostic.switchToLocationSettings();
+}
+
+function onResume(){
+    checkState();
+}
+
+
+$(document).on("deviceready", onDeviceReady);
